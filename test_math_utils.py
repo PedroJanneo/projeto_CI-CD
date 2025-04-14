9e8dd95e5b1ba00b21adf7b5e4522a22786f20ca
Pedro Janneo <165821981+PedroJanneo@users.noreply.github.com>
2025-04-14 20:14:47 -0300

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -3,7 +3,7 @@ from math_utils import add, subtract
 
 class TestMathUtils(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(5, 5), 10)
+        self.assertEqual(add(5, 6), 11)
         self.assertEqual(add(-1, 1), 0)
 
     def test_subtract(self):
